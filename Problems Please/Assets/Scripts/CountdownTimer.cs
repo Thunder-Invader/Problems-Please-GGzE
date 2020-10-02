41dd68eba07de8ee3cfda8d077e2f6e4e85dd5d7
Thunder-Invader <bard.mennen@gmail.com>
2020-10-02 17:39:16 +0200

    Created base UI
    Added dialogue option buttons, video player, timer, replay button and text panels

---
+++ Problems Please/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour {
+    public int duration;
+    private float countdown;
+    public Image timerVisuals;
+    public TMP_Text timerText;
+    public Gradient newColor;
+    public Gradient alpha;
+    public UnityEvent completed;
+
+
+    // Use this for initialization
+    void Start () {
+        Reset();
+        
+    }
+
+    public void Reset()
+    {
+        CancelInvoke();
+        countdown = duration;
+        InvokeRepeating("CountDown", 0, 0.1f);
+    }
+
+    public float GetCountdown()
+    {
+        return countdown;
+    }
+
+
+
+    private void CountDown()
+    {
+        countdown -= 0.1f;
+        UpdateUI();
+        if (countdown <= 0)
+        {
+            CancelInvoke();
+            completed.Invoke();
+        }
+    }
+
+    private void UpdateUI()
+    {
+        timerText.text = Mathf.Ceil(countdown).ToString();
+        timerText.color = newColor.Evaluate(countdown / duration);
+        timerVisuals.fillAmount = (countdown / duration);
+        timerVisuals.GetComponentsInChildren<Image>()[1].color = alpha.Evaluate(countdown / duration);
+    }
+}
