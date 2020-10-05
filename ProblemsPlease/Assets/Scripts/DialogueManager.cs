757ac1ad5fb37c2b0e597f802533ce65085d568c
Thunder-Invader <bard.mennen@gmail.com>
2020-10-05 21:16:44 +0200

    Added Video player script

--- ProblemsPlease/Assets/Scripts/DialogueManager.cs
+++ ProblemsPlease/Assets/Scripts/DialogueManager.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    [SerializeField]
+    private Text buttonLowText = default;
+
+    [SerializeField]
+    private Text buttonNeutralText = default;
+
+    [SerializeField]
+    private Text buttonHighText = default;
+
+    [SerializeField]
+    private Text buttonConfrontText = default;
+
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ ProblemsPlease/Assets/Scripts/DialoguePlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class DialoguePlayer : MonoBehaviour
+{
+    [SerializeField]
+    private VideoPlayer patientPlayer = default;
+
+    [SerializeField]
+    private UnityEvent freezeFrame = default;
+
+    private void Start()
+    {
+        patientPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    public void StartVideo()
+    {
+        // patientPlayer.Play();
+        StartCoroutine(Test());
+    }
+
+    public void OnVideoEnd(VideoPlayer vp)
+    {
+        freezeFrame.Invoke();
+    }
+
+    private IEnumerator Test()
+    {
+        yield return new WaitForSeconds(2);
+        OnVideoEnd(patientPlayer);
+    }
+}
