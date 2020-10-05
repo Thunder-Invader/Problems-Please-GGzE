f60bcb3042bbd7d40ad5272bfe0062af8d785607
Thunder-Invader <bard.mennen@gmail.com>
2020-10-05 22:33:01 +0200

    first version dialoguemanager script

--- ProblemsPlease/Assets/Scripts/DialogueManager.cs
+++ ProblemsPlease/Assets/Scripts/DialogueManager.cs
@@ -17,6 +17,20 @@ public class DialogueManager : MonoBehaviour
     [SerializeField]
     private Text buttonConfrontText = default;
 
+    [SerializeField]
+    private Text ConText = default;
+
+    [SerializeField]
+    private Text PreviousText = default;
+
+    [SerializeField]
+    private Text AnswerText = default;
+
+    //hardcoded
+
+    int stress = 2;
+    int minStress = 0;
+    int maxStress = 4;
 
     // Start is called before the first frame update
     void Start()
@@ -28,5 +42,16 @@ public class DialogueManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void AddStress(int amount)
+    {
+        stress += amount;
+        if (!(minStress < stress && maxStress > stress))
+        {
+            Debug.Log("Game Over");
+            return;
+        }
+        AnswerText.text = "stress: " + stress;
     }
 }
