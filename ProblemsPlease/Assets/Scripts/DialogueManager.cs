b297e2b0f294902599a6f08c2dfd8702d29d253f
Thunder-Invader <bard.mennen@gmail.com>
2020-10-12 14:21:42 +0200

    Added Mock-up script
    Added Mock-up script to load in the data into the json file.

--- ProblemsPlease/Assets/Scripts/DialogueManager.cs
+++ ProblemsPlease/Assets/Scripts/DialogueManager.cs
@@ -56,6 +56,7 @@ public class DialogueManager : MonoBehaviour
             return;
         }
         AnswerText.text = "stress: " + stress;
+
     }
 
     public void FreezeFrame()
---
+++ ProblemsPlease/Assets/Scripts/Smock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScenarioMock", menuName = "ScriptableObjects/ScenarioMockSO", order = 1)]
+public class Smock : ScriptableObject
+{
+    public ScenarioMock ScenarioMock;
+}
+
+[System.Serializable]
+public class ScenarioMock
+{
+    public string scenarioName;
+    public string scenarioDes;
+    public int maxStress; //min stress is 0
+    public int startStress;
+    public string failLowText;
+    public string failHighText;
+    public string winText;
+    public int score;
+}
+
+[System.Serializable]
+public class ScenarioPhase
+{
+    //Confrontation is saved here
+}
+
+[System.Serializable]
+public class ScenarioButton
+{
+    public string buttonText;
+    public bool buttonEnabled;
+    public string videoName;
+
+}
