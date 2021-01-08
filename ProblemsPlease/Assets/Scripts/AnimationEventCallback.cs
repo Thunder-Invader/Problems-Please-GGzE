29e37f8a70b85982f19d07403966915483a68bb4
Thunder-Invader <bard.mennen@gmail.com>
2021-01-08 17:37:13 +0100

    added EMDR videos

--- ProblemsPlease/Assets/Scripts/AnimationEventCallback.cs
+++ ProblemsPlease/Assets/Scripts/AnimationEventCallback.cs
@@ -8,9 +8,16 @@ public class AnimationEventCallback : MonoBehaviour
     [SerializeField]
     private UnityEvent callBack;
 
+    [SerializeField]
+    private UnityEvent callBackTwo;
 
     public void CallBack()
     {
         callBack.Invoke();
+    }
+
+    public void CallBack2()
+    {
+        callBackTwo.Invoke();
     }
 }
